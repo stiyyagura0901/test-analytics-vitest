2844fa7a858915c35dfbe29bf6d869e63d1f3e11
Samba siva Tiyyagura <40207674+stiyyagura0901@users.noreply.github.com>
2025-08-26 16:22:52 -0400

    flaky tests

---
+++ src/math.test.js
@@ -0,0 +1,20 @@
+const { sum } = require("./math.js");
+
+Array(5)
+  .fill(0)
+  .forEach((_, index) => {
+    it(`adds ${index} + 2 to equal ${index + 2}`, () => {
+      const isFlaky = Math.random() > 0.5;
+      expect(sum(index, 2)).toBe(isFlaky ? 100 : index + 2);
+    });
+  });
+
+it("adds 2 + 5 to equal 7", () => {
+  const isFlaky = Math.random() > 0.5;
+  expect(sum(2, 5)).toBe(isFlaky ? 100 : 7);
+});
+
+it("adds 2 + 6 to equal 8", () => {
+  const isFlaky = Math.random() > 0.5;
+  expect(sum(2, 6)).toBe(isFlaky ? 100 : 8);
+});
